d6a36b786f7f50e39b4dc1fe0389abab76c00dd7
hahoangnhat <hahoangnhat@icloud.com>
2024-12-07 20:37:11 +0700

    feat: update header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -12,9 +12,9 @@ const Header = ({ appName, utils }: IHeaderProps) => {
   const locale = useLocale()
 
   return (
-    <header className="flex justify-between px-5 py-1">
+    <header className="flex justify-between bg-hint-of-red-50 bg-opacity-20 px-5 py-1 text-white">
       <div className="flex items-center gap-5 text-sm">
-        <AppleIcon className="h-4 w-4" />
+        <AppleIcon className="h-4 w-4 text-white" />
 
         {/* Application utils */}
         <span className="font-semibold">{appName}</span>
@@ -24,10 +24,10 @@ const Header = ({ appName, utils }: IHeaderProps) => {
       </div>
 
       <div className="flex items-center gap-5 text-sm">
-        <BatteryIcon className="h-5 w-5" />
-        <WifiIcon className="h-4 w-4" />
-        <SearchIcon className="h-5 w-5" />
-        <SwitchIcon className="h-5 w-5" />
+        <BatteryIcon className="h-5 w-5 text-white" />
+        <WifiIcon className="h-4 w-4 text-white" />
+        <SearchIcon className="h-5 w-5 text-white" />
+        <SwitchIcon className="h-5 w-5 text-white" />
         <span>{getCurrentDateTime(locale as ELocale)}</span>
       </div>
     </header>
--- src/components/Icons/SearchIcon.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react'
-import { SVGProps } from 'react'
-const SvgComponent = (props: SVGProps<SVGSVGElement>) => (
-  <svg xmlns="http://www.w3.org/2000/svg" width={96} height={96} viewBox="0 0 64 64" {...props}>
-    <path d="M27 9C17.075 9 9 17.075 9 27s8.075 18 18 18c4.13 0 7.926-1.413 10.967-3.76l13.082 13.082a2.315 2.315 0 1 0 3.273-3.273L41.24 37.967C43.587 34.927 45 31.129 45 27c0-9.925-8.075-18-18-18zm0 4c7.719 0 14 6.281 14 14s-6.281 14-14 14-14-6.281-14-14 6.281-14 14-14z" />
-  </svg>
-)
-export default SvgComponent
