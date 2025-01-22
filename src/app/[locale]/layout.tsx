557b2c2e0d01231c976305e69626bb3fdbfc0d9c
nhathh-rdvn <nhathh.rdvn@gmail.com>
2025-01-22 15:39:39 +0700

    feat: add font jetbrains mono

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,6 +2,13 @@ import { StoreProvider } from '@/providers'
 import { NextIntlClientProvider } from 'next-intl'
 import { getLocale, getMessages } from 'next-intl/server'
 import React from 'react'
+import { JetBrains_Mono } from 'next/font/google'
+
+const jetBrainsMono = JetBrains_Mono({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-jetbrains',
+})
 
 const LocaleLayout = async ({ children }: { children: React.ReactNode }) => {
   const locale = await getLocale()
@@ -9,7 +16,7 @@ const LocaleLayout = async ({ children }: { children: React.ReactNode }) => {
 
   return (
     <html lang={locale}>
-      <body>
+      <body className={jetBrainsMono.variable}>
         <StoreProvider>
           <NextIntlClientProvider messages={messages}>{children}</NextIntlClientProvider>
         </StoreProvider>
